09263642ee4a253dd32841a72cae9f6806e419b7
Mikołaj Mazurkiewicz <mikolajmazurkiewicz.90@gmail.com>
2023-09-20 22:33:32 +0200

    update

--- R/auth.R
+++ R/auth.R
@@ -8,5 +8,5 @@ access_token <- jsonlite::fromJSON(Sys.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 names(access_token)
 
 library(gargle)
-options(gargle::gargle_oauth_cache = ".secrets")
+options(gargle_oauth_cache = ".secrets")
 gargle::gargle_oauth_cache()
